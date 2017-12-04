9287b57ac253d2e615825d950a8bdbc7a052b638
jonnykira <jonny.donnelly@kirasystems.com>
2017-12-04 15:52:01 -0500

    test script for evaluating the accuracy on binary sentiment tasks

---
+++ eval_test.py
@@ -0,0 +1,11 @@
+import eval_classification
+import openai.utils
+import openai.encoder
+
+
+encoder = openai.encoder.Model()
+
+# eval_nested_kfold(encoder, name, loc='./data/', k=10, seed=1234, use_nb=False)
+
+
+scores = eval_classification.eval_nested_kfold(encoder,name='CR',loc='/Users/jonathan/Downloads/data/customerr', use_nb=False)
