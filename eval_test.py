3dcbe8b8c4ff9ec9f0b467ee8f3934d834bf2ea2
jonnykira <jonny.donnelly@kirasystems.com>
2017-12-12 22:20:03 -0500

    test script for evaluating encoded representations on classification tasks

--- eval_test.py
+++ eval_test.py
@@ -7,5 +7,4 @@ encoder = openai.encoder.Model()
 
 # eval_nested_kfold(encoder, name, loc='./data/', k=10, seed=1234, use_nb=False)
 
-
 scores = eval_classification.eval_nested_kfold(encoder,name='CR',loc='/Users/jonathan/Downloads/data/customerr', use_nb=False)
